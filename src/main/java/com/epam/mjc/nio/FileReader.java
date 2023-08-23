2b5553a67e8248d270e094867468675f62376378
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-23 14:25:09 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -2,7 +2,6 @@ package com.epam.mjc.nio;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
