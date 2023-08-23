c4a83da603e1d6964625dece8452ea0aeac5c026
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-23 13:40:17 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,52 +1,52 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FileReader {
-    public static void main(String[] args) {
-        File file = new File("resources/Profile.txt");
-        Profile profile = FileReader.getDataFromFile(file);
 
-        if (profile != null) {
-            System.out.println("Name: " + profile.getName());
-            System.out.println("Age: " + profile.getAge());
-            System.out.println("Email: " + profile.getEmail());
-            System.out.println("Phone: " + profile.getPhone());
-        }
-    }
-    
-    public static Profile getDataFromFile(File file) {
-        try {
-            String content = new String(Files.readAllBytes(Paths.get(file.toURI())));
-
-            Map<String, String> profileData = parseProfileData(content);
-
-            String name = profileData.get("Name");
-            int age = Integer.parseInt(profileData.get("Age"));
-            String email = profileData.get("Email");
-            String phone = profileData.get("Phone");
-
-            return new Profile(name, age, email, phone);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null; // Handle error appropriately
-        }
+    public Profile getDataFromFile(File file) throws IOException {
+        String fileContent = readContentFromFile(file);
+        Map<String, String> profileData = parseContent(fileContent);
+        return createProfile(profileData);
     }
 
-    private static Map<String, String> parseProfileData(String content) {
-        Map<String, String> profileData = new HashMap<>();
-        String[] lines = content.split("\\r?\\n");
-
-        for (String line : lines) {
-            String[] parts = line.split(": ");
-            if (parts.length == 2) {
-                profileData.put(parts[0], parts[1]);
+    private String readContentFromFile(File file) throws IOException {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
             }
         }
+        return content.toString();
+    }
 
+    private Map<String, String> parseContent(String content) {
+        Map<String, String> profileData = new HashMap<>();
+        String[] lines = content.split("\n");
+        for (String line : lines) {
+            String[] keyValue = line.split(": ");
+            if (keyValue.length == 2) {
+                String key = keyValue[0].trim();
+                String value = keyValue[1].trim();
+                profileData.put(key, value);
+            }
+        }
         return profileData;
+    }
+
+    private Profile createProfile(Map<String, String> profileData) {
+        String name = profileData.get("Name");
+        Integer age = Integer.valueOf(profileData.get("Age"));
+        String email = profileData.get("Email");
+        Long phone = Long.valueOf(profileData.get("Phone"));
+        return new Profile(name, age, email, phone);
     }
 }
